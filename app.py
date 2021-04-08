da23d941c5b5b41fa523e9ea349cf4633e4b1a51
richard9809 <richard.ndisya@strathmore.edu>
2021-04-08 14:56:34 +0300

    init

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Richard Mulu',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Hello, Richard!"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1> About Page</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
